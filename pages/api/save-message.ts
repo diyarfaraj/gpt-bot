89705124af9e24a6ed6cb0eb6a0d1046ee0632c0
Diyar Faraj <diyar.faraj@gmail.com>
2023-04-11 02:11:19 +0200

    add  new info

--- pages/api/save-message.ts
+++ pages/api/save-message.ts
@@ -15,17 +15,24 @@ export default async function handler(
     }
 
     const container = client.database('DiyarBotDb').container('messages');
-    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    // const ipAddress: string | string[] | undefined =
+    // req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const ipAddress = '80.79.80.23:21671';
+
+    const regex: RegExp = /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/;
+    const matches: RegExpMatchArray | null | undefined = Array.isArray(
+      ipAddress,
+    )
+      ? ipAddress[0].match(regex)
+      : ipAddress?.match(regex);
+    const ip: string = matches ? matches[1] : '';
     const date = new Date().toISOString();
 
     try {
-      console.log(ip);
-      console.log(process.env.APIINFO_KEY);
-
       const response = await axios.get(
-        `https://ipinfo.io/${'ip'}?token=${process.env.APIINFO_KEY}`,
+        `https://ipinfo.io/${ip}?token=${process.env.APIINFO_KEY}`,
       );
-      const { city, region, country, loc } = response.data;
+      const { city, region, country, loc, postal, org } = response.data;
 
       await container.items.create({
         message,
@@ -35,6 +42,8 @@ export default async function handler(
         region,
         country,
         loc,
+        postal,
+        org,
       });
       res.status(201).json({ success: true });
     } catch (error) {
